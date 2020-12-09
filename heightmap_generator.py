76843b5b769af9bdf3c92ccdbda1ae1916643102
Martin <martinmunck@yahoo.com>
2020-12-09 17:12:09 +0200

    Added a Perlin Noise algorithm tweaked for our needs, might need some more work

--- heightmap_generator.py
+++ heightmap_generator.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 # pip install pypng
+# pip install noise
+# pip install numpy
+import numpy as np
 import png
 import random
+import math
+from noise import pnoise2
 
 
 def generate_heightmap(height=2017, width=2017, greyscale=True, bitdepth=16, algorithm="thousand needles",
@@ -13,12 +18,15 @@ def generate_heightmap(height=2017, width=2017, greyscale=True, bitdepth=16, alg
 
     if algorithm == "thousand needles":
         image = thousandNeedles(image, height, width, thhighpoints, thdotsize, thsmoothing, maxval)
+    elif algorithm == "perlin":
+        image = perlin(image, height, width, maxval)
 
     with open(save_loc, "wb") as file:
         w = png.Writer(height=height, width=width, greyscale=greyscale, bitdepth=bitdepth)
         w.write(file, image)
 
-def thousandNeedles(image, height, width, thhighpoints, thdotsize, thsmoothing, maxval):
+
+def thousandNeedles(image, height, width, thhighpoints=300, thdotsize=30, thsmoothing=40, maxval=pow(2, 16) - 1):
     print("creating Thousand Needles Heightmap")
 
     for i in range(thhighpoints):
@@ -40,4 +48,28 @@ def thousandNeedles(image, height, width, thhighpoints, thdotsize, thsmoothing, 
         # print(image[rndheight][rndwidth])
         print("progress at ", step + 1, " out of ", thsmoothing)
 
+    return image
+
+#Code was inspired by and based on the blog post https://engineeredjoy.com/blog/perlin-noise/
+def perlin(image, height, width, maxval=pow(2, 16) - 1, scale=400, octaves=30, persistence=0.2, lacunarity=2,
+           seed=None):
+    if not seed:
+        seed = np.random.randint(0, 100)
+        print("creating Perlin Noise heightmap, seed was {}".format(seed))
+    for i in range(height):
+        for j in range(width):
+            image[i][j] = pnoise2(i / scale,
+                                  j / scale,
+                                  octaves=octaves,
+                                  persistence=persistence,
+                                  lacunarity=lacunarity,
+                                  repeatx=1024,
+                                  repeaty=1024,
+                                  base=seed)
+    max_arr = np.max(image)
+    min_arr = np.min(image)
+
+    for i in range(len(image)):
+        for j in range(len(image[0])):
+            image[i][j] = abs(math.floor(image[i][j] * maxval))
     return image

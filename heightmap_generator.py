2815398bf4bb736c69e290539fe47a2e44d1214d
rauno <raunoita@gmail.com>
2020-12-10 01:41:14 +0200

    Added diamond-square to algorithms

--- heightmap_generator.py
+++ heightmap_generator.py
@@ -8,6 +8,8 @@ import png
 import random
 import math
 from noise import pnoise2
+from itertools import chain
+from copy import deepcopy
 
 
 def generate_heightmap(height=2017, width=2017, greyscale=True, bitdepth=16, algorithm="thousand needles",
@@ -20,6 +22,8 @@ def generate_heightmap(height=2017, width=2017, greyscale=True, bitdepth=16, alg
         image = thousandNeedles(image, height, width, thhighpoints, thdotsize, thsmoothing, maxval)
     elif algorithm == "perlin":
         image = perlin(image, height, width, maxval)
+    elif algorithm == "diamondsquare":
+        image = diamond_square(height, bitdepth)
 
     with open(save_loc, "wb") as file:
         w = png.Writer(height=height, width=width, greyscale=greyscale, bitdepth=bitdepth)
@@ -50,7 +54,8 @@ def thousandNeedles(image, height, width, thhighpoints=300, thdotsize=30, thsmoo
 
     return image
 
-#Code was inspired by and based on the blog post https://engineeredjoy.com/blog/perlin-noise/
+
+# Code was inspired by and based on the blog post https://engineeredjoy.com/blog/perlin-noise/
 def perlin(image, height, width, maxval=pow(2, 16) - 1, scale=400, octaves=30, persistence=0.2, lacunarity=2,
            seed=None):
     if not seed:
@@ -73,3 +78,102 @@ def perlin(image, height, width, maxval=pow(2, 16) - 1, scale=400, octaves=30, p
         for j in range(len(image[0])):
             image[i][j] = abs(math.floor(image[i][j] * maxval))
     return image
+
+
+def diamond(heightmap, magnitude, max_val):
+    size = len(heightmap)
+    new_heightmap = deepcopy(heightmap)
+    for p1_y in range(size):
+        for p1_x in range(size):
+            if heightmap[p1_y][p1_x] != "_____":
+                for p2_x in range(p1_x+2, size):
+                    if heightmap[p1_y][p2_x] != "_____":
+                        side = p2_x-p1_x
+                        if p1_y + side <= size:
+                            new_heightmap[int(p1_y+side/2)][int(p1_x+side/2)] = round(max(min(((heightmap[p1_y][p1_x]
+                                                                                                + heightmap[p1_y][p2_x]
+                                                                                                + heightmap[p1_y + side][p1_x]
+                                                                                                + heightmap[p1_y + side][p2_x])
+                                                                                               / 4
+                                                                                               + random.uniform(0, magnitude)),
+                                                                                              max_val), 0))
+                        break
+    return new_heightmap
+
+
+def square(heightmap, magnitude, max_val):
+    size = len(heightmap)
+    new_heightmap = deepcopy(heightmap)
+    for p1_y in range(size):
+        for x in range(size):
+            if heightmap[p1_y][x] != "_____":
+                node_unhandled = True
+                for p2_y in range(p1_y+2, size):
+                    if heightmap[p2_y][x] != "_____":
+                        radius = int((p2_y - p1_y) / 2)
+                        underflow_x = x - radius
+                        overflow_x = x + radius
+                        if underflow_x < 0:
+                            underflow_x -= 1
+                        if overflow_x > size-1:
+                            overflow_x -= size-1
+                        new_heightmap[p1_y + radius][x] = round(max(min(((heightmap[p1_y][x]
+                                                                          + heightmap[p2_y][x]
+                                                                          + heightmap[p1_y+radius][underflow_x]
+                                                                          + heightmap[p1_y+radius][overflow_x])
+                                                                         / 4
+                                                                        + random.uniform(0, magnitude)), max_val), 0))
+                        node_unhandled = False
+                        break
+                if node_unhandled and p1_y < size-1:
+                    radius = size-1 - p1_y
+                    underflow_x = x - radius
+                    if underflow_x < 0:
+                        underflow_x -= 1
+                    overflow_x = x + radius
+                    if overflow_x > size - 1:
+                        overflow_x -= size - 1
+                    underflow_y = size-1
+                    overflow_y = 0
+                    new_val = round(max(min(((heightmap[p1_y][x] * 2
+                                              + heightmap[overflow_y][underflow_x]
+                                              + heightmap[overflow_y][overflow_x])
+                                             / 4
+                                             + random.uniform(-magnitude, magnitude)), max_val), 0))
+                    new_heightmap[overflow_y][x] = new_val
+                    new_heightmap[underflow_y][x] = new_val
+    return new_heightmap
+
+
+# Arbitrary positive integer size, seeds between 0 and 1
+def diamond_square(size, bitdepth, seed_min=0.17, seed_max=0.83, magnitude=0.2, magnitude_reduction=0.6):
+    max_val = 2**bitdepth - 1
+
+    # Data preprocessing
+    dimension = 1 + (1 << (size - 1).bit_length())
+    seed1 = round(random.uniform(seed_min * max_val, seed_max * max_val))
+    seed2 = round(random.uniform(seed_min * max_val, seed_max * max_val))
+    seed3 = round(random.uniform(seed_min * max_val, seed_max * max_val))
+    seed4 = round(random.uniform(seed_min * max_val, seed_max * max_val))
+
+    # Heightmap generation
+    heightmap = [["_____" for i in range(dimension)] for j in range(dimension)]
+    heightmap[0][0] = seed1
+    heightmap[0][dimension-1] = seed2
+    heightmap[dimension-1][0] = seed3
+    heightmap[dimension-1][dimension-1] = seed4
+
+    state_is_diamond = True
+    magnitude = magnitude * max_val
+    while "_____" in chain(*heightmap):
+        if state_is_diamond:
+            heightmap = diamond(heightmap, magnitude, max_val)
+        else:
+            heightmap = square(heightmap, magnitude, max_val)
+        state_is_diamond = not state_is_diamond
+        magnitude = magnitude * magnitude_reduction
+
+    # Padding removal
+    heightmap = [i[0:size] for i in heightmap[0:size]]
+
+    return heightmap
